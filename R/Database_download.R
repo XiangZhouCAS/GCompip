3bc9b5b78871b86dae7d3636c786a80e6c8d6403
XiangZhouCAS <147217990+XiangZhouCAS@users.noreply.github.com>
2025-03-20 16:20:19 +0800

    Add files via upload

---
+++ R/Database_download.R
@@ -0,0 +1,25 @@
+#' Download database.
+#'
+#' @param db_filepath Download the USCG database and Hydrogenase, hydrogen metabolism terminal enzyme database.
+#' @param db_type Select the type of database to download, default is "USCG", Other types include "ter", "hyd" and "all".
+#' @return The database
+#' @export
+#'
+#' @examples Database_download(db_filepath = "your/database/filepath",db_type = "USCG")
+Database_download <- function(db_filepath,db_type = "USCG"){
+  if(db_type == "USCG"){
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/Ribo_14.dmnd"))
+    print("USCG database has been downloaded.")}
+  if(db_type == "hyd"){
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/hyd_id-name.script.txt"))
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/hyddb.all.dmnd"))
+    print("Hydrogense database and hyd_id-name.script.txt have been downloaded.")}
+  if(db_type == "ter"){
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/ter.dmnd.gz"))
+    print("Terminal database has been downloaded.")}
+  if(db_type == "all"){
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/Ribo_14.dmnd"))
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/hyd_id-name.script.txt"))
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/hyddb.all.dmnd"))
+    system(sprintf("wget -c https://github.com/XiangZhouCAS/GCompip/raw/refs/heads/main/database/ter.dmnd.gz"))
+    print("All databases have been downloaded.")}}
